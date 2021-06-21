72f031fd460542673294fe3718ad65bd6e824481
Doug Boari <doug@tastymushroom.com>
2021-06-21 17:06:56 +0200

    Initial check in of plant carrier repo.

---
+++ libplantcarrier/README.md
@@ -0,0 +1,4 @@
+# libplantcarrier
+
+This library is for code that is specific to the plant-carrier.
+An example class (PlantCarrierDemo) and corresponding tests are included.
---
+++ libplantcarrier/src/PlantCarrierDemo.cpp
@@ -0,0 +1,7 @@
+#include "PlantCarrierDemo.h"
+
+namespace plant_imager {
+    int32_t PlantCarrierDemo::Demo_function(int32_t num1, int32_t num2) {
+            return num1 + num2;
+    }
+}
